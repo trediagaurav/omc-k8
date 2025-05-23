a37a5c87e9efe69f8ca453ea6488c013eec9eb48
Gaurav <tgaurav@cambium.com>
2025-05-24 00:21:10 +0300

    deleting all

--- modules/nodegroup/outputs.tf
+++
@@ -1,9 +0,0 @@
-# modules/nodegroup/outputs.tf
-
-output "node_group_role_arn" {
-  value = aws_iam_role.node_group_role.arn
-}
-
-output "node_group_name" {
-  value = aws_eks_node_group.ng.node_group_name
-}
